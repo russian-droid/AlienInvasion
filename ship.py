970cd79cb72edc2bec3d6c3639975d7327654200
Igor the roofCoder <53712180+russian-droid@users.noreply.github.com>
2021-02-25 23:11:27 -0700

    Update ship.py

--- ship.py
+++ ship.py
@@ -5,16 +5,17 @@ class Ship:
 
     def __init__(self, ai_game):
         """ initialize the ship and set its starting position """
+        #assign the screen to an attribute of Ship , so we can access it easily in all the methods in this class
         self.screen = ai_game.screen
         self.screen_rect = ai_game.screen.get_rect()
 
-        #load the ship image and get its rect
-        self.image = pygame.image.load('images/ship.bpm')
+        #load the ship image from a folder and get its rect
+        self.image = pygame.image.load('images/ship.bmp')
         self.rect = self.image.get_rect()
 
         #start each new ship at the bottom center of the screen
         self.rect.midbottom = self.screen_rect.midbottom
 
-    def blitime(self):
-        """ Draw the ship at this current location """
+    def blitme(self):
+        """ Draw the ship at this current location on screen """
         self.screen.blit(self.image, self.rect)
