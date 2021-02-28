b369c82a4e0a673f0a8405ef14b7f627e84a2cd9
Igor the roofCoder <53712180+russian-droid@users.noreply.github.com>
2021-02-27 23:38:52 -0700

    Update bullet.py

--- bullet.py
+++ bullet.py
@@ -1,6 +1,8 @@
 import pygame
 from pygame.sprite import Sprite
 
+#class inherits from Sprite , which we import from the pygame .sprite module.
+#When you use sprites, you can group related elements in your game and act on all the grouped elements at once.
 class Bullet(Sprite):
     """A class to manage bullets fired from the ship"""
 
@@ -12,8 +14,21 @@ class Bullet(Sprite):
         self.color = self.settings.bullet_color
 
         # Create a bullet rect at (0, 0) and then set correct position.
+        #bullet is actually just a rect
+        #coordinates from top left corner
         self.rect = pygame.Rect(0, 0, self.settings.bullet_width,self.settings.bullet_height)
         self.rect.midtop = ai_game.ship.rect.midtop
 
         # Store the bullet's position as a decimal value.
         self.y = float(self.rect.y)
+
+    def update(self):
+        """Move the bullet up the screen."""
+        # Update the decimal position of the bullet.
+        self.y -= self.settings.bullet_speed
+        # Update the rect position.
+        self.rect.y = self.y
+
+    def draw_bullet(self):
+        """Draw the bullet to the screen."""
+        pygame.draw.rect(self.screen, self.color, self.rect)
