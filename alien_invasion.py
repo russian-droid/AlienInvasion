90c5f147102c259474f1e61dd666f7a499a5968a
Igor the roofCoder <53712180+russian-droid@users.noreply.github.com>
2021-03-04 20:05:27 -0700

    Update alien_invasion.py

--- alien_invasion.py
+++ alien_invasion.py
@@ -35,14 +35,18 @@ class AlienInvasion:
         while True:
             self._check_events()
             self.ship.update()
-            self.bullets.update()
+            self._update_bullets()
             self._update_screen()
-
-            # Get rid of bullets that have disappeared.
-            for bullet in self.bullets.copy():
-                if bullet.rect.bottom <= 0:
-                    self.bullets.remove(bullet)
-            #print(len(self.bullets)) #just to check that bullets disappear
+            
+    def _update_bullets(self):
+        """Update position of bullets and get rid of old bullets."""
+        # Update bullet positions.
+        self.bullets.update()
+        # Get rid of bullets that have disappeared.
+        for bullet in self.bullets.copy():
+            if bullet.rect.bottom <= 0:
+                self.bullets.remove(bullet)
+        #print(len(self.bullets)) #just to check that bullets disappear
 
     def _check_events(self):
         """ Respond to keypresses and mouse events """
@@ -79,8 +83,9 @@ class AlienInvasion:
 
     def _fire_bullet(self):
         """Create a new bullet and add it to the bullets group."""
-        new_bullet = Bullet(self)
-        self.bullets.add(new_bullet)
+        if len(self.bullets) < self.settings.bullets_allowed:
+            new_bullet = Bullet(self)
+            self.bullets.add(new_bullet)
 
     def _update_screen(self):
         """ update images on the screen, and flip to the new screen """
